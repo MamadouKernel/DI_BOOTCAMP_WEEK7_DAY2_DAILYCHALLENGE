f7d28f1c54da24955f5180da88180de99d3d68d8
Konate Mamadou <120653410+MamadouKernel@users.noreply.github.com>
2023-02-06 11:12:12 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+
+/*
+*@author Konate-MK
+*/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
+
+
+/*
+*@author Konate-MK
+*/
